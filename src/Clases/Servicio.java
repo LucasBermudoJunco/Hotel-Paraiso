97a259facc0edecb0b37422561716e3188dc5f09
LucasBermudoJunco <lucasbjunco@gmail.com>
2024-05-08 20:10:06 +0200

    Arranque del Proyecto

--- src/Clases/Servicio.java
+++ src/Clases/Servicio.java
@@ -1,5 +1,49 @@
 package Clases;
 
 public class Servicio {
+	
+	private int id;
+	private double precio;
+	private String tipo;
+	
+	public Servicio() {
+		super();
+	}
+
+	public Servicio(int id, double precio, String tipo) {
+		super();
+		this.id = id;
+		this.precio = precio;
+		this.tipo = tipo;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public double getPrecio() {
+		return precio;
+	}
+
+	public void setPrecio(double precio) {
+		this.precio = precio;
+	}
+
+	public String getTipo() {
+		return tipo;
+	}
+
+	public void setTipo(String tipo) {
+		this.tipo = tipo;
+	}
+
+	@Override
+	public String toString() {
+		return "Servicio [id=" + id + ", precio=" + precio + ", tipo=" + tipo + "]";
+	}
 
 }
